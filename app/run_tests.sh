33a7479a09a62df42df053f1496def1744596cd9
root <root@vps199039.ovh.net>
2018-06-22 22:46:37 +0200

    create a provisional testing cron

--- app/run_tests.sh
+++ app/run_tests.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-
-FILES=$(find $PWD/tests/Feature -name "*.php")
-while IFS= read -r line
-do
-  echo "test to run:  $line"
-  vendor/bin/phpunit $line
-done <<< "$FILES"
-
-
-FILES=$(find $PWD/tests/Unit -name "*.php")
-while IFS= read -r line
-do
-  echo "test to run:  $line"
-  vendor/bin/phpunit $line
-done <<< "$FILES"
+while : ;
+  do
+    FILES=$(find $PWD/tests/Feature -name "*.php")
+    while IFS= read -r line
+    do
+      echo "test to run:  $line"
+      vendor/bin/phpunit $line
+    done <<< "$FILES"
+    
+    
+    FILES=$(find $PWD/tests/Unit -name "*.php")
+    while IFS= read -r line
+    do
+      echo "test to run:  $line"
+      vendor/bin/phpunit $line
+    done <<< "$FILES"
+    
+    sleep 60
+  
+done
