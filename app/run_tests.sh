5267226195cdf2e032d152d9ca26ff66a8ce23f1
root <root@vps199039.ovh.net>
2018-06-22 23:19:13 +0200

    reformat cron

--- app/run_tests.sh
+++ app/run_tests.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-while : ;
-  do
-    FILES=$(find $PWD/tests/Feature -name "*.php")
-    while IFS= read -r line
-    do
-      echo "test to run:  $line"
-      vendor/bin/phpunit $line
-    done <<< "$FILES"
-    
-    
-    FILES=$(find $PWD/tests/Unit -name "*.php")
-    while IFS= read -r line
-    do
-      echo "test to run:  $line"
-      vendor/bin/phpunit $line
-    done <<< "$FILES"
-    
-    sleep 60
-  
-done
+
+FILES=$(find $PWD/tests/Feature -name "*.php")
+while IFS= read -r line
+do
+  echo "test to run:  $line"
+  vendor/bin/phpunit $line
+done <<< "$FILES"
+
+
+FILES=$(find $PWD/tests/Unit -name "*.php")
+while IFS= read -r line
+do
+  echo "test to run:  $line"
+  vendor/bin/phpunit $line
+done <<< "$FILES"
